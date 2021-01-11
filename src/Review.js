374a10f6f216c9a0be7a98007ff69bb72e82bd27
rebeca-august <rebeca.cazacu@gmail.com>
2021-01-11 18:13:03 +0000

    Add functionality to the Review Component

--- src/Review.js
+++ src/Review.js
@@ -2,9 +2,40 @@ import React, { useState } from 'react'
 import people from './data'
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
+const randomIntFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 const Review = () => {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
+
+  const prevPerson = () => {
+    if (index > 0) {
+      setIndex(index - 1)
+    } else {
+      setIndex(people.length - 1)
+    }
+  }
+
+  const nextPerson = () => {
+    if (index < people.length - 1) {
+      setIndex(index + 1)
+    } else {
+      setIndex(0)
+    }
+  }
+
+  const randomPerson = () => {
+    let randomNumber = randomIntFromInterval(0, people.length - 1)
+
+    while (randomNumber === index) {
+      console.log(randomNumber)
+      randomNumber = randomIntFromInterval(0, people.length - 1)
+    }
+
+    setIndex(randomNumber)
+  }
 
   return (
     <article className="review">
@@ -18,14 +49,16 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">Surprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        Surprise me
+      </button>
     </article>
   )
 }
